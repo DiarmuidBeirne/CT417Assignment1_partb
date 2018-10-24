c166188bd17ccab7b1d8aeee1a746ecba74767da
Diarmuid Beirne <d.beirne.8@gmail.com>
2018-10-24 17:22:06 +0100

    Finished Console Outputs

--- src/main/java/driver.java
+++ src/main/java/driver.java
@@ -22,57 +22,134 @@ public class driver {
         LocalDate dob4 = new LocalDate(1998, 05,02);
         LocalDate dob5 = new LocalDate(1990, 12,30);
 
-        //Create Modules Students
+        //Create Mock Students
         Student student1 = new Student("Eamon",dob1,001);
         Student student2 = new Student("Cormac",dob2,002);
         Student student3 = new Student("Marc",dob3,003);
         Student student4 = new Student("Alan",dob4,004);
-        Student student5 = new Student("Mac",dob5,005);
+        Student student5 = new Student("Mack",dob5,005);
 
-        //Create Mock Modules
-        Module module1 = new Module("Soft Eng 3", 4001);
-        Module module2 = new Module("Graphics & Img", 4002);
-        Module module3 = new Module("Programming", 4003);
-
-        //Create Mock Course
-        LocalDate courseStartDate = new LocalDate(2018,9,01);
-        LocalDate courseEndDate = new LocalDate(2018, 12, 02);
-        Course course1 = new Course("4BCT",courseStartDate,courseEndDate);
-
-        //Add Modules to Course
-        ArrayList<Module> mockModuleList = new ArrayList<Module>();
-        mockModuleList.add(module1);
-        mockModuleList.add(module2);
-        mockModuleList.add(module3);
-        course1.setModuleList(mockModuleList);
-
-        //Add Students to Modules
-        //Add individual students to a module
-        module1.addStudent(student1);
-        module1.addStudent(student2);
-        module1.addStudent(student3);
-        module2.addStudent(student4);
-        module2.addStudent(student5);
-        //Create a list of students and assign List to module
-        ArrayList<Student> studentList = new ArrayList<Student>();
+        //add the students to a list
+       ArrayList<Student> studentList = new ArrayList<Student>();
         studentList.add(student1);
         studentList.add(student2);
         studentList.add(student3);
         studentList.add(student4);
         studentList.add(student5);
 
-        module3.setStudents(studentList);
+        //Create Mock Modules
+        Module module1 = new Module("Soft Eng 3", 4001);
+        Module module2 = new Module("Graphics & Img", 4002);
+        Module module3 = new Module("Programming", 4003);
+
+        //Create Mock Courses
+        LocalDate courseStartDate = new LocalDate(2018,9,01);
+        LocalDate courseEndDate = new LocalDate(2018, 12, 02);
+        Course course1 = new Course("4BCT",courseStartDate,courseEndDate);
+        Course course2 = new Course("4BA",courseStartDate,courseEndDate);
+        ArrayList<Course> courseList = new ArrayList<Course>();
+        courseList.add(course1);
+        courseList.add(course2);
+        //Assign Courses to Students
+
+            addStudentToCourse(student1, course1);
+            addStudentToCourse(student2, course2);
+            addStudentToCourse(student3, course2);
+            addStudentToCourse(student4, course1);
+            addStudentToCourse(student5, course1);
 
 
-        //Print Modules assigned to course
-        ArrayList<Module> moduleList;
-        moduleList = course1.getModules();
 
-
-        for (Module m : moduleList) {
-            System.out.println(m.getModuleName());
+        //Add Modules to Course
+        ArrayList<Module> mockModuleList = new ArrayList<Module>();
+        mockModuleList.add(module1);
+        mockModuleList.add(module2);
+        mockModuleList.add(module3);
+        for(Module m : mockModuleList)
+        {
+            addModuleToCourse(m, course1);
+        }
+        //Add only 2 modules to second course
+        mockModuleList.remove(module1);
+        for(Module m : mockModuleList)
+        {
+            addModuleToCourse(m, course2);
         }
 
 
+
+        //Add Students to Modules
+        addStudentToModule(student1, module1);
+        addStudentToModule(student1, module2);
+        addStudentToModule(student2, module2);
+        addStudentToModule(student2, module3);
+        addStudentToModule(student3, module2);
+        addStudentToModule(student3, module3);
+        addStudentToModule(student4, module1);
+        addStudentToModule(student4, module2);
+        addStudentToModule(student4, module3);
+        addStudentToModule(student5, module1);
+        addStudentToModule(student5, module2);
+
+
+
+
+
+        //Print Modules assigned to each course with student Details
+
+        ArrayList<Module> moduleList;
+
+
+
+        for(Course c : courseList) {
+            moduleList = c.getModules();
+            System.out.println("\n\n" + c.getCourseName() + "\n\n");
+            System.out.println("Modules:");
+            for (Module m : moduleList) {
+                System.out.println("\nModule Name:\t" + m.getModuleName());
+                System.out.println("Students Registered");
+                for (Student s : m.getStudents()) {
+                    System.out.println(s.getFullName() + "\tUsername: " + s.getUsername() + "\tCourse: " + s.getEnrolledCourse().getCourseName());
+                }
+
+            }
+        }
+
+        //Print a list of All Students with their courses and Modules
+        System.out.println("\n\n List of All Students Registered \n\n");
+        for(Student s : studentList){
+            System.out.println("Name: \t" + s.getFullName());
+            System.out.println("Username: \t" + s.getUsername());
+            System.out.println("Course: \t" + s.getEnrolledCourse().getCourseName());
+            ArrayList<Module>  studentModules = s.getModuleList();
+            System.out.println("Modules Registered");
+            for(Module m : studentModules)
+            {
+                System.out.println(m.getModuleName());
+            }
+            System.out.println("\n\n");
+
+        }
+
+
+
+    }
+
+    private static void addStudentToCourse(Student s, Course course)
+    {
+        s.setEnrolledCourse(course);
+        course.addStudent(s);
+    }
+
+    private static void addStudentToModule(Student s, Module module)
+    {
+        s.addModule(module);
+        module.addStudent(s);
+    }
+
+    private static void addModuleToCourse(Module m, Course c)
+    {
+        m.addCourse(c);
+        c.addModule(m);
     }
 }
